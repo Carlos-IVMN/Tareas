ca51f707b0fa2a7bed224f3f009d856aeca85216
Carlos Morales <carlos.moralesestrada@galileo.edu>
2021-02-07 19:54:27 -0600

    agrega el archivo readme

---
+++ TareaUnidad2.js
@@ -0,0 +1,13 @@
+var mes = 10
+var mes = prompt("Porfavor ingrese el mes")
+var valor = parseInt(mes)
+alert("El mes es " + valor)
+if (valor <= 2 || valor == 12)
+  alert("Es invierno")
+else if (valor >= 3 && valor <= 5)
+  alert("Es primavera")
+else if (valor >= 6 && valor <= 8)
+  alert("Es verano")
+else if (valor >= 9 && valor <= 11)
+  alert("Es otono")
+alert("numero de carne 21005174 - nombre Carlos Alberto Morales Estrada")
